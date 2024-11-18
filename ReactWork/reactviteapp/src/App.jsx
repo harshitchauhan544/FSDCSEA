0f5129f463ac881ce2a7ca96a67fc09492381be0
harshitchauhan544 <harshitchauhan544@gmail.com>
2024-11-18 13:03:40 +0530

    18-11

--- ReactWork/reactviteapp/src/App.jsx
+++ ReactWork/reactviteapp/src/App.jsx
@@ -1,17 +1,58 @@
 import React from "react";
 import Student from "./Student";
+import logo from "./p1.jpg"
+import StudentState from "./StudentState";
 
 function App(){
   let a = 12
   const mystyle = {
     backgroundColor:'pink',
-    color:'cyan',
-    height:'500px'
+    color:'blue',
+    height:'500px',
+    display:'flex'
   }
+
+  const studentdata = [
+    {
+    college:"ABES Engineering College",
+    pic: "https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_4.png",
+    name:"Jaadu",
+    branch:"Arts",
+    roll:"21"
+    },
+    {
+      college:"ABES Engineering College",
+      pic: "https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_4.png",
+      name:"Jaadu",
+      branch:"Arts",
+      roll:"21"
+      },
+      {
+        college:"ABES Engineering College",
+        pic: "https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_4.png",
+        name:"kullu",
+        branch:"Arts",
+        roll:"21"
+        },
+        {
+          college:"ABES Engineering College",
+          pic: "https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_4.png",
+          name:"Jaadu",
+          branch:"Arts",
+          roll:"21"
+          },
+  ]
   return (
-    <div style = {mystyle}> ABES Engineering College
-      <Student name="SO"></Student>
-    </div>
+    // <div style = {mystyle}> ABES Engineering College
+    //   {/* <Student name="SO" roll = "66" branch = "CSE" pic = {<img src ={logo} height ={100} widht ={120}/>}></Student> */}
+    //   {/* <Student name="NO" roll = "68" branch = "CS" pic = {<img src ={logo} height ={100} widht ={120}/>}></Student> */}
+    //   {/* <Student  roll = "69" branch = "CS" pic = {<img src ={logo} height ={100} widht ={120}/>}></Student> */}
+    //   <Student data={studentdata[0]}/>
+    //   <Student data={studentdata[1]}/>
+    //   <Student data={studentdata[2]}/>
+    //   <Student data={studentdata[3]}/>
+    // </div>
+    <StudentState/>
 
     // <div> ABES Engineering College </div>
     // <div style = mystyle></div>
--- ReactWork/reactviteapp/src/Student.jsx
+++ ReactWork/reactviteapp/src/Student.jsx
@@ -1,20 +1,40 @@
 import React from "react";
-import './Student.css'
+import "./Student.css"
 function Student(props){
+    // console.log(props.data.pic);
+    
     return (
-        <div className = 'icard'>
+        <div className ='icard'>
+            {/* {
+                JSON.stringify(props.data)
+            } */}
             <table>
                 <tbody>
+
                     <tr>
-                        <th>Roll No.</th><th>Name</th><th>Branch</th>
+                        <td colSpan='2'>{props.data.college}</td>
                     </tr>
                     <tr>
-                        <td>01</td><td>{props.name}</td><td>Cse</td>
+                        <td colSpan='2' ><img src={props.data.pic} height = "100" width="150" /></td>
+                    </tr>
+                    <tr>
+                        <td>Name</td><td>{props.data.name}</td>
+                    </tr>
+                    <tr>
+                        <td>Roll no</td><td>{props.data.roll}</td>
+                    </tr>
+                    <tr>
+                        <td>Branch</td><td>{props.data.branch}</td>
                     </tr>
                 </tbody>
             </table>
         </div>
     )
+}
+
+Student.defaultProps={
+    name :"Jaadu",
+
 }
 
 export default Student
